832d3a6dc324d38ef5c6f31a9f9be8c907a75760
Razvan <olaeru.razvan@gmail.com>
2015-06-23 18:42:00 +0300

    #12 [CS] [A] View product list Implemented grid layout.

--- customer-site/cs-android/UltrashopMobile/app/src/main/java/app/pentastagiu/ro/ultrashopmobile/ProductAdapter.java
+++ customer-site/cs-android/UltrashopMobile/app/src/main/java/app/pentastagiu/ro/ultrashopmobile/ProductAdapter.java
@@ -1,6 +1,10 @@
 package app.pentastagiu.ro.ultrashopmobile;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -38,33 +43,74 @@ public class ProductAdapter extends ArrayAdapter<Product> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
-        try {
-            ProductHolder holder = new ProductHolder();
+        ProductHolder holder = new ProductHolder();
 
-            //first verify if convertView is not null
-            if (convertView == null) {
-                //this is a new view we inflate the new layout
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(R.layout.row_layout, null);
-                // now we can fill the layout with the right values
-                TextView productNameView = (TextView) view.findViewById(R.id.name);
-                TextView productPriceView = (TextView) view.findViewById(R.id.price);
+        //first verify if convertView is not null
+        if (convertView == null) {
+            //this is a new view we inflate the new layout
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.row_layout, null);
+            // now we can fill the layout with the right values
+            TextView productNameView = (TextView) view.findViewById(R.id.name);
+            TextView productPriceView = (TextView) view.findViewById(R.id.price);
+            ImageView imageView = (ImageView) view.findViewById(R.id.imgProduct);
 
-                holder.productNameView = productNameView;
-                holder.priceView = productPriceView;
+            holder.productNameView = productNameView;
+            holder.priceView = productPriceView;
+            holder.imageView = imageView;
 
-                view.setTag(holder);
-            } else {
-                holder = (ProductHolder) view.getTag();
-            }
-
-            Product product = productList.get(position);
-            holder.productNameView.setText(product.getName());
-            holder.priceView.setText("" + product.getPrice());
-        } catch (Exception e) {
-            Log.e("PADPT", "Error: " + e);
+            view.setTag(holder);
+        } else {
+            holder = (ProductHolder) view.getTag();
         }
+
+        Product product = productList.get(position);
+        holder.productNameView.setText(product.getName());
+        holder.priceView.setText("" + product.getPrice());
+        new DownloadImageTask(holder.imageView).execute("http://192.168.108.213:90/images/" + product.getId() + "/1.jpg");
         return view;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Bitmap temp = BitmapFactory.decodeResource(null, R.mipmap.ic_launcher);
+            bmImage.setImageBitmap(temp);
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap productImage = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                productImage = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("imgError", "Error downloading image: ", e);
+            }
+            return productImage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+    public void swapProductList(List<Product> products) {
+        clear();
+
+        for (Product object : products) {
+            add(object);
+        }
+
+        notifyDataSetChanged();
     }
 
     /* *********************************
