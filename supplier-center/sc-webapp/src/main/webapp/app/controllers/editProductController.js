0ceda9d6489e7e272d098494898147d8abb6016c
unknown <dlesnic@P5RO-PORT264.pentalog.com>
2015-07-27 10:35:13 +0300

    #57 [SC][FE] Bug fix <select> over 1000 options - implement Lazy loading

--- supplier-center/sc-webapp/src/main/webapp/app/controllers/editProductController.js
+++ supplier-center/sc-webapp/src/main/webapp/app/controllers/editProductController.js
@@ -18,17 +18,11 @@ app.controller('editProductController', [
 			function getAllSuppliers() {
 				supplierFactory.getAllSuppliers().success(function(suppliers) {
 					$scope.suppliers = suppliers;
-					// Find supplier by id.This function is used to set the
-					// default supplier in the edit product selector
-					// var supplierId = productFactory.getProduct().supplier.id;
-					// $scope.supplier = $filter('filter')($scope.suppliers, {
-					// id : supplierId
-					// })[0];
 				});
 			}
 			;
 			function updateProduct() {
-				delete $scope.order.product._uiSelectChoiceDisabled;
+				delete $scope.product._uiSelectChoiceDisabled;
 				productFactory.updateProduct($scope.product).success(
 						function() {
 							$location.path('/products');
